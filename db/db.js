6a3361a1eabdfa2e6dc53b0271c56e02e7f1c355
joeyklee <jsphknglee@gmail.com>
2020-01-06 15:09:28 -0500

    adds basic express app

---
+++ db/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+const MONGODB_URI = config.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser:true, 
+  useCreateIndex: true, useUnifiedTopology: true});
---
+++ public/main.js
@@ -0,0 +1,1 @@
+console.log('hello lovely human')
