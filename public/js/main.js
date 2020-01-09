bb6f968b1bb7ae691c7061be5799c0476ab38fb7
joeyklee <jsphknglee@gmail.com>
2020-01-09 15:58:43 -0500

    rm files

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded", function(){
-  console.log('hello lovely human');
-  const posts = new Posts();
-  const login = new Login();
-  const signup = new Signup();
-});
