63bcf42a9e8cf5f4f9835646b470e4675c51936b
joeyklee <jsphknglee@gmail.com>
2020-01-09 16:23:57 -0500

    adds auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const config = require('../config');
+
+const auth = async (req, res, next) => {
+
+  try{
+
+    let token;
+
+    if(req.cookies.auth_token ){
+      token = req.cookies.auth_token;
+    } else if(req.header('Authorization')) {
+      token = req.header('Authorization').replace('Bearer ', '');  
+    } else{
+      throw new Error('no token given')
+    }
+
+
+    const data = jwt.verify(token, config.JWT_KEY)
+    const user = await User.findOne({_id: data._id, 'tokens.token':token})
+
+    if(!user){
+      throw new Error({error: 'error logging in'})
+    }
+
+    req.user = user;
+    req.token = token;
+
+    next();
+  } catch(err){
+    res.status(401).send({ error: 'Not authorized to access this resource' })
+  }
+}
+
+module.exports = auth;
