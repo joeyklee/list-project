5b63f3ff5004ccad45dc455f30040dfdd748d6d1
joeyklee <jsphknglee@gmail.com>
2020-01-09 16:40:32 -0500

    adds authd posts routes

---
+++ middleware/isOwner.js
@@ -0,0 +1,18 @@
+const Posts = require('../models/post');
+
+const isOwner = async (req, res, next) => {
+  try{
+    const id = req.params.id; 
+    const data = await Posts.findById(id);
+
+    if(String(data.createdBy_id) === String(req.user._id)){
+      next();
+    } else {
+      throw new Error('Sorry, you are not the owner of that resource');
+    }
+  } catch(err){
+    res.status(401).send({ error: 'Not authorized to edit or delete this resource' })
+  }
+}
+
+module.exports = isOwner;
